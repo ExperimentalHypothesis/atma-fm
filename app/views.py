0d002535269c8a7a70020483dc010fc616fe628b
nirvikalpa <experimentalhypothesis@gmail.com>
2019-12-01 22:34:57 +0100

    funguje na lokalu - prehrava jeden song pri prekliknut na /radio

---
+++ app/views.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+import miniaudio
+
+
+@app.route("/")
+def hello():
+	return render_template("mainpage/main.html")
+
+
+
+
+def play():
+	stream = miniaudio.stream_file(r"C:\Users\nirvikalpa\source\repos\radio\app\static\audio\09_Volven_-_Your_World_In_My_Eyes.mp3")
+	device = miniaudio.PlaybackDevice()
+	device.start(stream)
+
+
+
+@app.route("/radio")
+def play_track() -> None:
+	play()
+	return render_template("radiopage/radio.html")
