832c9a4c3830cad8e88b96247dc153172c0e9ba1
nirvikalpa <experimentalhypothesis@gmail.com>
2019-12-06 23:42:33 +0100

    nekonecka smycka, hraje ale nacita furt jakoby novou page

--- app/views.py
+++ app/views.py
@@ -1,20 +1,44 @@
 from app import app
 from flask import render_template
-import miniaudio, os
+
+import random, time, vlc, os, random
+
+
+
+path = r"C:\Users\nirvikalpa\Desktop\2000 volven"
+file = random.choice(os.listdir(path))
+full_name = path + "\\" + file
+
+def play_random_song(song):
+    vlc_instance = vlc.Instance()
+    player = vlc_instance.media_player_new()
+    media = vlc_instance.media_new(song)
+    player.set_media(media)
+    player.play()
+    file = random.choice(os.listdir(path))
+    time.sleep(1.5)
+    print(f"Duration of this song is {player.get_length()/1000} seconds")
+    time.sleep(player.get_length()/1000)  
+
+
+""" 
+while True:
+    file = random.choice(os.listdir(path))
+    full_name = path + "\\" + file
+    print(f"currently playing {full_name}")    
+    play_random_song(full_name)
+ """
+
 
 @app.route("/")
 def hello():
 	return render_template("mainpage/main.html")
 
-
-
-def play():
-	# stream = miniaudio.stream_file(send_from_directory("app\static\audio" ,"09_Volven_-_Your_World_In_My_Eyes.mp3"))
-	stream = miniaudio.stream_file(r"C:\Users\nirvikalpa\source\repos\radio\app\static\audio\09_Volven_-_Your_World_In_My_Eyes.mp3")
-	device = miniaudio.PlaybackDevice()
-	device.start(stream)
-
 @app.route("/radio")
-def play_track() -> None:
-	play()
+def play_track() -> None:	
+	while True:
+		file = random.choice(os.listdir(path))
+		full_name = path + "\\" + file
+		print(f"currently playing {full_name}")    
+		play_random_song(full_name)
 	return render_template("radiopage/radio.html")
