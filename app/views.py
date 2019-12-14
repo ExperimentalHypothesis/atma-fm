c229c5264601069860fbac51e0cba79ec1c2f269
nirvikalpa <experimentalhypothesis@gmail.com>
2019-12-14 10:45:44 +0100

    plays in infinite loop using cycle - but only ATOI

--- app/views.py
+++ app/views.py
@@ -1,21 +1,37 @@
 from app import app
 from flask import render_template, Response
-import random, time, vlc, os, random
+import random, time, os, random, vlc
+from itertools import cycle
 
-path = r"C:\Users\nirvikalpa\Desktop\Ambient Temple Of Imagination\MY$TERY SCHOOL\ATOI - Planetary House Nation (Vol.5) - 1997"
+# folder = r"C:\Broadcast\Ambient Temple Of Imagination"
+# one_album_path = r"C:\Users\nirvikalpa\Music\Ambient Temple Of Imagination\ATOI - Gaia (Vol.4) - 1996"
+# one_album_playlist = [os.path.join(one_album_path, i) for i in os.listdir(one_album_path) if i.endswith(".mp3")]
+# # print(random.shuffle(one_album_playlist))
+
+
+root = r"C:\Broadcast\Ambient Temple Of Imagination"
+playlist = []
+for path, dirs, files in os.walk(root):
+	for file in files:
+		if file.endswith(".mp3"):
+			playlist.append(os.path.abspath(os.path.join(path,file)))
+random.shuffle(playlist)
+
+print(playlist)
+
 
 @app.route("/")
 def main():
 	return render_template("mainpage/main.html")
 
-def gen():
-	for i in os.listdir(path):
-		with open(path + "\\" + i, "rb") as f:
-			data = f.read(1024)
-			while data:
-				yield data
-				data = f.read(1024)
-				
 @app.route("/stream")
 def stream():
+	def gen():
+		for song in cycle(playlist):
+			with open(song, "rb") as f:
+				print(song) # DEBUG
+				data = f.read(1024)
+				while data:
+					yield data
+					data = f.read(1024)
 	return Response(gen(), mimetype="audio/mp3")
