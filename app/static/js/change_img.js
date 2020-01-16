3516fe0fe667685420480d5c30564176e5d37e07
nirvikalpa <experimentalhypothesis@gmail.com>
2020-01-16 11:06:43 +0100

    set up for streaming via icecast backend server

--- app/static/js/change_img.js
+++
@@ -1,9 +0,0 @@
-
-var imgSRC={'src1':'app\static\img\play.png','src2': 'app\static\img\pause.png' }
-
-$('.image1').toggle(function(){
-      $(this).attr('src',imgSRC.src2)
-   }, function() {
-       $(this).attr('src',imgSRC.src1)
-   }
-});
