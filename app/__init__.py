1a9c32ecb1a28a5c02a463d89f8a89aa249d03bd
lukas <experimentalhypothesis@gmail.com>
2020-01-30 15:07:19 +0100

    slight chenges in naming

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import views
+from app import routes
--- app/helper_scripts/normalize_audio.py
+++ app/helper_scripts/normalize_audio.py
@@ -1,35 +1,52 @@
 # prerequisites:
-# 1] install ffmpeg
-# 2] install ffmpeg-normalize
+# 1] install ffmpeg and add to path
+# 2] install ffmpeg-normalize [pip install ..]
 
-import os
+import os, subprocess
 
-def normalize_audiofile(path_to_file: str):
-""" normalmizes volume and loudness of files """
+def check_bitrate(root: str, min_bitrate: bytes) -> dict:
+  """ make a map of files having less bitrate than specified """
 
-  from subprocess import check_output
-  if path_to_file.endswith(".flac"):
-    command = f'ffmpeg-normalize "{path_to_file}" -o "{path_to_file.strip(".flac")}.mp3" -c:a mp3 -b:a 192k'
-    check_output(command, shell=True)
-  # elif path_to_file.endswith(".mp3"):
-  #   command = f'ffmpeg-normalize "{path_to_file}" -o "{path_to_file}" -c:a mp3 -b:a 192k'
-  #   check_output(command, shell=True)
+  with open(r"C:\Users\lukas\source\repos\python\online-radio\app\helper_scripts\audio_extensions.txt") as f:
+      e = f.read().splitlines()
 
-
-
-def normalize_audioalbum(path_to_dir: str):
-
-  #extension = ('.mp3', '.flac')
-  for path, dirs, files in os.walk(path_to_dir):
-    for file in files:
-      if not file.endswith(".flac"):
-        continue
-      else:
-        normalize_audiofile(os.path.abspath(os.path.join(path, file)))
+  command = "ffprobe -v error -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1"
+  d = {}
+  for path, dirs, files in os.walk(root):
+      for file in files:
+          if file.endswith(tuple(e)):
+              file_name = os.path.join(path, file)
+              full_command = "".join(command + f' "{file_name}"')
+              out = subprocess.check_output(full_command)
+              if out < min_bitrate:
+                   d[file_name] = out
+  return d
 
 
-path_to_music_dir = r"C:\Users\nirvikalpa\Music"
-normalize_audiofile(path_to_music_dir)
+#TODO udelat aby to bralo commandline argumenty jako cesty odkud kam
+def normalize_audio(source: str) -> None:
+  with open(r"C:\Users\lukas\source\repos\python\online-radio\app\helper_scripts\audio_extensions.txt") as f:
+    exts = f.read().splitlines()
+  norma = " [peak norm -5]"
+  #ffmpeg_command = f'ffmpeg-normalize "{filepath_old}" -o "{filepath_new}" -c:a mp3 -ar 44100'
+
+  for path, _, files in os.walk(source):
+      for file in files:
+          if file.endswith(".mp3"):
+              filepath_source = os.path.join(path, file)
+              index = filepath_source.rfind(".")
+              filepath_norm = filepath_source[:index] + norma + filepath_source[index:]
+              filepath_target = filepath_norm.replace("audio", "normalized test") #TODO tadyto prepsat aby to bylo univerzalnejsi pri commandline argumentech
+              head, _ = os.path.split(filepath_target)
+              if os.path.exists(head):
+                  print(f'normalizing "{filepath_source}" saving to "{filepath_target}" ')
+                  subprocess.run(f'ffmpeg-normalize "{filepath_source}" -nt peak -t -5 -o "{filepath_target}" -c:a libvorbis -ar 44100')
+              else:
+                  os.makedirs(head)
+                  print(f'normalizing "{filepath_source}" saving to "{filepath_target}" ')
+                  subprocess.run(f'ffmpeg-normalize "{filepath_source}" -nt peak -t -5 -o "{filepath_target}" -c:a libvorbis -ar 44100')
 
 
-
+if __name__ == "__main__":
+  source = r"\\192.168.0.109\lukas\online radio resources\audio - tohle se testuje pro normalizaci"
+  normalize_audio(source)
