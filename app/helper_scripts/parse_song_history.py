da1da6604451ff4c1ff4362471faa57b3a867ff5
nirvikalpa <experimentalhypothesis@gmail.com>
2020-02-18 16:35:59 +0100

    add routes, contact and playlist working

---
+++ app/helper_scripts/parse_song_history.py
@@ -0,0 +1,38 @@
+# was used as a testing  module for getting song history data from remote server 
+
+
+# def read_file_from_remote(host: str, user: str, pwd: str) -> list:
+# 	""" reads the playlist of last n songs from remote linux server """
+
+# 	import paramiko
+# 	ssh_client = paramiko.SSHClient()
+# 	ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # this is to validate the clients machine
+# 	try:
+# 		ssh_client.connect(hostname=host, username=user, password=pwd)
+# 	except Exception as e:
+# 		print(e) # TODO na frontend
+# 	else:
+# 		song_history = []
+# 		path_to_file = r"/var/log/icecast/icegen1.log"
+# 		sftp_client = ssh_client.open_sftp()
+# 		remote_file = sftp_client.open(path_to_file)
+# 		try:
+# 			for line in list(remote_file)[-7:]:
+# 				song_history.append(line)
+# 			return reversed(song_history) 
+# 		except Exception as e:
+# 			print(e) # TODO na frontend
+# 		finally:
+# 			remote_file.close()
+
+
+# def parse_song_history(playlist: list) -> list:
+# 	""" parses the playlist """
+
+# 	parsed_list = []
+# 	for line in playlist:
+# 		if "Rotating queues and looping again..." in line:
+# 			continue
+# 		else:
+# 			parsed_list.append(line.replace("2020: Now playing ", ' -- ').strip(".mp3\n").lower())
+# 	return parsed_list
--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,23 @@
 import requests
 from app import app
 from flask import render_template, Response
-import random, time, os, random, vlc
-from itertools import cycle
+import random, time, os, random
 
 @app.route("/")
-def main():
-	return render_template("mainpage/main.html")
+def home():
+	return render_template("index.html")
 
-# def read_file_from_remote(host: str, user: str, pwd: str) -> list:
-# 	""" reads the playlist of last n songs from remote linux server """
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
-# 	import paramiko
-# 	ssh_client = paramiko.SSHClient()
-# 	ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # this is to validate the clients machine
-# 	try:
-# 		ssh_client.connect(hostname=host, username=user, password=pwd)
-# 	except Exception as e:
-# 		print(e) # TODO na frontend
-# 	else:
-# 		song_history = []
-# 		path_to_file = r"/var/log/icecast/icegen1.log"
-# 		sftp_client = ssh_client.open_sftp()
-# 		remote_file = sftp_client.open(path_to_file)
-# 		try:
-# 			for line in list(remote_file)[-7:]:
-# 				song_history.append(line)
-# 			return reversed(song_history) 
-# 		except Exception as e:
-# 			print(e) # TODO na frontend
-# 		finally:
-# 			remote_file.close()
+@app.route("/contact")
+def contact():
+	return render_template("contact.html")
 
-
-# def parse_song_history(playlist: list) -> list:
-# 	""" parses the playlist """
-
-# 	parsed_list = []
-# 	for line in playlist:
-# 		if "Rotating queues and looping again..." in line:
-# 			continue
-# 		else:
-# 			parsed_list.append(line.replace("2020: Now playing ", ' -- ').strip(".mp3\n").lower())
-# 	return parsed_list
-
+@app.route("/archive")
+def archive():
+	return render_template("archive.html")
 
 @app.route("/playlist")
 def playlist():
@@ -53,5 +27,5 @@ def playlist():
 					("jun 13, 08:56 2020", "ambientum", "silence is noisy", "creative"),
 					("jun 13, 09:22 2020", "radiohead", "the bends", "kid a"),
 					("jun 13, 09:34 2020", "voice of eye", "rubadurs", "voice in the air"),]*3
-	return render_template("playlist/playlist.html", song_history=song_history)
+	return render_template("playlist.html", song_history=song_history)
 
