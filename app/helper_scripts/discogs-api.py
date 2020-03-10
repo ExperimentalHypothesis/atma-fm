edc6a3120066a2b3881da2f757e0871a5d1dcf24
lukas <lukas@localhost.localdomain>
2020-03-10 21:42:14 +0100

    discogs api check albums - not ready

--- app/helper_scripts/discogs-api.py
+++ app/helper_scripts/discogs-api.py
@@ -5,7 +5,7 @@ def get_releases_from_local_filesystem(source:str) -> list:
     """ return list of releases from filesystem 
         the format of namedtuple(artist, album, [songs]) """
 
-    Release = namedtuple("Release", ["author", "album", "songs"])
+    Release = namedtuple("Release", ["artist", "album", "songs"])
     discographies = []
 
     audio_extensions = os.path.join(os.path.dirname(__file__), "audio_extensions.txt")
@@ -20,45 +20,86 @@ def get_releases_from_local_filesystem(source:str) -> list:
                 releases = []
                 songs = []
                 if os.path.isdir(os.path.join(composer_path, album)):
-                    releases.append(composer)
-                    releases.append(album) 
+                    comp = composer.replace("-", " ")
+                    releases.append(comp)
+                    alb = album.replace("-", " ")
+                    releases.append(alb) 
                     album_path = os.path.join(composer_path, album)
                     for song in os.listdir(album_path):
                         if os.path.isfile(os.path.join(album_path, song)) and song.endswith(tuple(e)):
-                            songs.append(song)
-                r = Release(composer, album, songs)
+                            song = " ".join(song.replace("-"," ").split()[1:])
+                            song = song.split(".")[0].strip(" ")
+                            songs.append(song.title())
+                r = Release(comp, alb, songs)
                 discographies.append(r)
     return(discographies)
 
 
-def get_release_versions_from_discogs_api(release_name:str) -> list: 
-    """ return list of all versions of a particular release specified in argument
-        the format is a namedtuple(artist, album, [songs]) """
+    # def get_release_versions_from_discogs_api(release_name:str) -> list: 
+    #     """ return list of all versions of a particular release specified in argument
+    #         the format is a namedtuple(artist, album, [songs]) """
+
+    #     Release = namedtuple("API_Release", ["api_artist", "api_album", "api_songs"])
+    #     releases = []
+        
+    #     try:
+    #         d = discogs_client.Client('atma-fm', user_token="XqVXtxTvsRtYoPaxmvqIfBXHKxyZEqlTVYVzvDPe")
+    #         versions = d.search(release_name, type='release')
+    #     except Exception as e:
+    #         print("error when connection to API", e)
+    #     else:
+    #         for version in versions:
+    #             songs = []
+    #             for track in version.tracklist:
+    #                 songs.append(track.title)
+    #                 r = Release(version.artists[0].name,version.title, songs) 
+    #             releases.append(r)
+    #         return releases
+
+
+def get_release_versions_from_discogs_api_new(r:namedtuple) -> list:
+    local_artist, local_album, local_tracklist = r.artist.title(), r.album.title(), r.songs
+
+    print(local_album,local_artist, local_tracklist)
 
     Release = namedtuple("API_Release", ["api_artist", "api_album", "api_songs"])
-    releases = []
-    
+    versions = [] 
     try:
         d = discogs_client.Client('atma-fm', user_token="XqVXtxTvsRtYoPaxmvqIfBXHKxyZEqlTVYVzvDPe")
-        versions = d.search(release_name, type='release')
+        releases = d.search(local_artist, type='artist')[0].releases
+    except IndexError:
+        print(f"Artist: {local_artist} not found on discogs.. skipping")
     except Exception as e:
-        print("error when connection to API", e)
+        print("error when connection to API:", e)
     else:
-        for version in versions:
-            songs = []
-            for track in version.tracklist:
-                songs.append(track.title)
-                r = Release(version.artists[0].name,version.title, songs) 
-            releases.append(r)
-        return releases
+        if releases:
+            for release in releases:
+                if local_album == release:
+                    # songs = []
+                    # for track in release.tracklist:
+                    #     songs.append(track.title)
+                    #     r = Release(local_artist, release.title, songs) 
+                    # versions.append(r)
+                    print(f"Album: {local_album} from Artist: {local_artist} found on discogs")
+                else:
+                    print(f"Album: {local_album} from Artist: {local_artist} not found on discogs")
+                    continue
+        # return versions
+
+
 
 
 
 if __name__ == "__main__":
     root="/run/media/lukas/MULTIMEDIA"
+    local_releases = get_releases_from_local_filesystem(root)
 
-    api_releases = get_release_versions_from_discogs_api("AaΔzhyd China Doll")
-    local_releases = get_releases_from_local_filesystem()
-    
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(api_releases)
+    for i in local_releases:
+        get_release_versions_from_discogs_api_new(i)
+
+
+    # api_releases = get_release_versions_from_discogs_api_new(local_releases[0])
+
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(api_releases)
+
