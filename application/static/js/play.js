301448fac5cd5b09fc02c854e8a0d55a1961829f
ExperimentalHypothesis <experimentalhypothesis@gmail.com>
2022-01-29 18:55:37 +0100

    responsive player box

--- application/static/js/play.js
+++ application/static/js/play.js
@@ -5,21 +5,21 @@ const listen_now_icon = document.getElementById("bt-icon")
 
 function toggle_play() {
   if (!is_playing) {
-    audio.play(); 
+    audio.play();
     listen_now_txt.innerHTML = "stop now";
-    listen_now_icon.className = "fas fa-volume-mute fa-3x";
+    listen_now_icon.className = "fas fa-volume-mute player-button-icon";
   } else {
-    audio.pause();    
+    audio.pause();
     listen_now_txt.innerHTML = "listen now";
-    listen_now_icon.className = "fa fa-volume-up fa-3x";
+    listen_now_icon.className = "fa fa-volume-up player-button-icon";
     audio.currentTime = 0;
   }
 };
 
-audio.onplaying = function() {
+audio.onplaying = function () {
   is_playing = true;
 };
 
-audio.onpause = function() {
+audio.onpause = function () {
   is_playing = false;
 };
