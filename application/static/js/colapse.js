aa989894dcae430933cf10e231e02e6894d70cbe
ExperimentalHypothesis <experimentalhypothesis@gmail.com>
2022-01-30 13:04:28 +0100

    all menu pages working well

--- application/static/js/colapse.js
+++ application/static/js/colapse.js
@@ -21,6 +21,15 @@ function closeArchive() {
     document.getElementById("archive").style.height = "0%";
 }
 
+
+function openPlaylist() {
+    document.getElementById("playlist").style.height = "50%";
+}
+function closePlaylist() {
+    document.getElementById("playlist").style.height = "0%";
+}
+
+
 function openAbout() {
     document.getElementById("about").style.height = "50%";
 }
