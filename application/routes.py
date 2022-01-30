5e912d9ec5cf9a1ed98d03fad213e880e7877f4c
ExperimentalHypothesis <experimentalhypothesis@gmail.com>
2022-01-30 13:17:14 +0100

    delete all routes from routes.py except index /

--- application/routes.py
+++ application/routes.py
@@ -4,31 +4,3 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/home")
-def home():
-	return render_template("index.html")
-
-@app.route("/about")
-def about():
-	return render_template("under-construction.html.html")
-
-@app.route("/channels")
-def channels():
-	return render_template("under-construction.html")
-
-@app.route("/playlists")
-def playlists():
-	return render_template("under-construction.html")
-
-@app.route("/archive")
-def archive():
-	return render_template("under-construction.html")
-
-@app.route("/contact")
-def contact():
-	return render_template("under-construction.html")
-
-@app.route("/support")
-def support():
-	return render_template("under-construction.html")
