e6d9de4804be9591d131c7db6fb60372b2d502b9
ExperimentalHypothesis <experimentalhypothesis@gmail.com>
2022-01-30 20:49:34 +0100

    fix getCurrentSong for long titles

--- application/utils/contextFunctions.py
+++ application/utils/contextFunctions.py
@@ -2,16 +2,22 @@ from flask import current_app as app
 
 
 def getCurrentSong() -> dict:
+    MAX_LEN_CURRENT_PLAY = 45
+
     if app.config["FLASK_ENV"] == "development":
         return dict(author="name of author", title="name of title")
     else:
         try:
             with open("/opt/ices/log/channel1/ices.cue") as f:
                 lines = [line for line in f]
-                title = lines[-2].lower()
-                author = lines[-1].lower()
-                print(title, author)
+                author = lines[-2].lower()
+                title = lines[-1].lower()
+                both = author +"|"+ title
+                if len(both) > MAX_LEN_CURRENT_PLAY:
+                    both = both[0:MAX_LEN_CURRENT_PLAY]
+                    author = both.split("|")[0]
+                    title = both.split("|")[1] + "..."
         except Exception as e:
-            print(e)
-            return dict(author="name of author", title="name of title")
-        return dict(author=author, title=title)
+            return dict(author="unknown artists", title="unknown song")
+
+        return dict(title=title, author=author)
